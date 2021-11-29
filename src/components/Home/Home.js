8d2453cc1f6b583758e2de541070d5b37bc46591
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-11-29 23:26:04 +0600

    protfolio initial stage complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Md.Naimur Rahman</strong>
+                <strong className="main-name"> Md.Naimur Rahman Rafi</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
